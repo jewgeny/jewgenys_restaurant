453d695948089345b72bbdcf962930409d3fc74c
jewgeny <jewgeny@gmx.net>
2019-03-07 09:36:58 +0100

    push build folder

---
+++ build/precache-manifest.797d6ee193d2ddc95cc3b1c5d9745a28.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af83cea5681f2b00e908aafaf60b2da",
+    "url": "/static/media/shrimp.0af83cea.jpg"
+  },
+  {
+    "revision": "72af95082dbff8df5790",
+    "url": "/static/css/main.6b6d01be.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f794583d7e023211ce8f41b6464a2e30",
+    "url": "/static/media/bg.f794583d.jpg"
+  },
+  {
+    "revision": "cecd5e634dc495d4a151",
+    "url": "/static/js/2.cecd5e63.chunk.js"
+  },
+  {
+    "revision": "2451effb74ae3a90f721e330c55b9583",
+    "url": "/static/media/chef.2451effb.jpg"
+  },
+  {
+    "revision": "506be3e03a7ee0e22b7722f85a64e618",
+    "url": "/static/media/chicken.506be3e0.jpg"
+  },
+  {
+    "revision": "1dfd7793ece4c7802035ddc432e710fd",
+    "url": "/static/media/fruit.1dfd7793.jpg"
+  },
+  {
+    "revision": "e36f83b6cc1d296aa161e60acaca1428",
+    "url": "/static/media/shrimp.e36f83b6.jpg"
+  },
+  {
+    "revision": "f57bf95324ae2eeeb6bf1e1186d1658b",
+    "url": "/static/media/tuna.f57bf953.jpg"
+  },
+  {
+    "revision": "11fbe6808d671f8de8b0d120c67fb116",
+    "url": "/static/media/veggie.11fbe680.jpg"
+  },
+  {
+    "revision": "2df3c6fb90934347410bb334a731437f",
+    "url": "/static/media/bacon.2df3c6fb.jpg"
+  },
+  {
+    "revision": "4cf08b0f38011f52ce6ddaa9ec6c0c10",
+    "url": "/static/media/cheese.4cf08b0f.jpg"
+  },
+  {
+    "revision": "6b081792b57c9ac7d80fc1a004aa4bfe",
+    "url": "/static/media/mumbai.6b081792.jpg"
+  },
+  {
+    "revision": "72af95082dbff8df5790",
+    "url": "/static/js/main.72af9508.chunk.js"
+  },
+  {
+    "revision": "a87713759a48b3a277036ea7f6652cee",
+    "url": "/static/media/smoky.a8771375.jpg"
+  },
+  {
+    "revision": "02628c26f1943206651febe740d56f73",
+    "url": "/static/media/veggie.02628c26.jpg"
+  },
+  {
+    "revision": "07a5b4586dfe18a4d2be83010dc7e69c",
+    "url": "/static/media/salami.07a5b458.jpg"
+  },
+  {
+    "revision": "6b20428573a4fccc495d9faf9a98ade8",
+    "url": "/static/media/beans.6b204285.jpg"
+  },
+  {
+    "revision": "63be2d07211f4f3742a0b5bb0c74c61c",
+    "url": "/static/media/champignons.63be2d07.jpg"
+  },
+  {
+    "revision": "3a18fa1001d57635fa8478e5e1202e7c",
+    "url": "/static/media/chicken.3a18fa10.jpg"
+  },
+  {
+    "revision": "f57135a988334b2e868bf85e93568a09",
+    "url": "/static/media/tomato.f57135a9.jpg"
+  },
+  {
+    "revision": "3191b0e2d2eb733a0a8f7725de8b8854",
+    "url": "/static/media/oliven.3191b0e2.jpg"
+  },
+  {
+    "revision": "445478443645432a99c872b02d462657",
+    "url": "/static/media/pizzas.44547844.jpg"
+  },
+  {
+    "revision": "e05f2e864bac17303908551d1a850829",
+    "url": "/static/media/burgers.e05f2e86.jpg"
+  },
+  {
+    "revision": "7ed88980111cc41f2341a84cc2f3655a",
+    "url": "/static/media/salads.7ed88980.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cecd5e634dc495d4a151",
+    "url": "/static/css/2.01ea6706.chunk.css"
+  },
+  {
+    "revision": "a48badc61b9e2e67c15b2b0974df4012",
+    "url": "/index.html"
+  }
+];
